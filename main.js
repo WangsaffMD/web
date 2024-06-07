18dcc8b736d56cb1d744123d6ccf9f9a27bd38ac
Wangsaff <134561473+WangsaffMD@users.noreply.github.com>
2024-06-08 00:10:13 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,23 +8,8 @@ function submitData() {
     let role = document.getElementById("role").value;
     let id = document.getElementById("ID").value;
 
-    if (
-        nama === "" ||
-        umur === "" ||
-        kota === "" ||
-        wr === "" ||
-        rr === "" ||
-        role === ""
-    ) {
-        alert("Please fill in all the fields.");
-    } else if (!isNaN(id)) {
-        alert("ID must be a number.");
-    } else if (!isNaN(rank) && !isNaN(rank.split(" ")[1])) {
-        alert("Rank must be a number followed by text.");
-    } else {
-        let url = `https://wa.me/6281234567890?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
-        location.replace(url);
-    }
+    let url = `https://wa.me/6281234567890?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
+    location.replace(url);
 }
 
 function submitData2() {
@@ -36,22 +21,6 @@ function submitData2() {
     let rank = document.getElementById("rank").value;
     let role = document.getElementById("role").value;
     let id = document.getElementById("ID").value;
-
-    if (
-        nama === "" ||
-        umur === "" ||
-        kota === "" ||
-        wr === "" ||
-        rr === "" ||
-        role === ""
-    ) {
-        alert("Please fill in all the fields.");
-    } else if (!isNaN(id)) {
-        alert("ID must be a number.");
-    } else if (!isNaN(rank) && !isNaN(rank.split(" ")[1])) {
-        alert("Rank must be a number followed by text.");
-    } else {
-        let url = `https://wa.me/6289876543210?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
-        location.replace(url);
-    }
+    let url = `https://wa.me/6289876543210?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
+    location.replace(url);
 }
