294e2290cf251cb46837279cd67f3847899ae312
Wangsaff <134561473+WangsaffMD@users.noreply.github.com>
2024-06-08 00:03:52 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getValue() {
+function submitData() {
     let nama = document.getElementById("name").value;
     let umur = document.getElementById("umur").value;
     let kota = document.getElementById("kota").value;
@@ -7,20 +7,37 @@ function getValue() {
     let rank = document.getElementById("rank").value;
     let role = document.getElementById("role").value;
     let id = document.getElementById("ID").value;
-    /*
-    let allResults = [
-        `nama : ${result1}`,
-        `umur : ${result2}`,
-        `asal : ${result3}`,
-        `Rank Tertinggi : ${result5}`,
-        `Rank saat ini : ${result6}`,
-        `Role : ${result7}`,
-        `ID : ${result8}`
-    ];
-    const test = allResults.toString();
-    */
-    //console.log(test);
-    const submit = `https://wa.me/6285697102235?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rank}%0aRank Saat Ini : ${rr}%0aRole : ${role}%0aID ML : ${id}`;
-    location.replace(submit);
-    //window.open(submit, "_blank");
+
+    if (nama === "" || umur === "" || kota === "" || wr === "" || rr === "" || role === "") {
+        alert("Please fill in all the fields.");
+    } else if (!isNaN(id)) {
+        alert("ID must be a number.");
+    } else if (!isNaN(rank) && !isNaN(rank.split(" ")[1])) {
+        alert("Rank must be a number followed by text.");
+    } else {
+        let url = `https://wa.me/6281234567890?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
+        location.replace(url);
+    }
+}
+
+function submitData2() {
+    let nama = document.getElementById("name").value;
+    let umur = document.getElementById("umur").value;
+    let kota = document.getElementById("kota").value;
+    let wr = document.getElementById("WR").value;
+    let rr = document.getElementById("RR").value;
+    let rank = document.getElementById("rank").value;
+    let role = document.getElementById("role").value;
+    let id = document.getElementById("ID").value;
+
+    if (nama === "" || umur === "" || kota === "" || wr === "" || rr === "" || role === "") {
+        alert("Please fill in all the fields.");
+    } else if (!isNaN(id)) {
+        alert("ID must be a text.");
+    } else if (!isNaN(rank) && !isNaN(rank.split(" ")[1])) {
+        alert("Rank must be a number followed by text.");
+    } else {
+        let url = `https://wa.me/6289876543210?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
+        location.replace(url);
+    }
 }
