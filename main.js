eee083c84a08d3023d6847a2d2f4646d91d50713
Wangsaff <134561473+WangsaffMD@users.noreply.github.com>
2024-06-10 19:27:10 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function submitData() {
+function submitData(phoneNumber) {
     let nama = document.getElementById("name").value;
     let umur = document.getElementById("umur").value;
     let kota = document.getElementById("kota").value;
@@ -8,19 +8,29 @@ function submitData() {
     let role = document.getElementById("role").value;
     let id = document.getElementById("ID").value;
 
-    let url = `https://wa.me/6285697102235?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
+    if (!nama ||!umur ||!kota ||!wr ||!rr ||!rank ||!role ||!id) {
+        alert("isi semua forms yang disediakan");
+        return;
+    }
+
+    if (!/^[a-zA-Z0-9 ]+$/.test(rank)) {
+        alert("isi rank sama bintang");
+        return;
+    }
+
+    if (!/^[a-zA-Z0-9 ]+$/.test(rr)) {
+        alert("isi rank sama bintang");
+        return;
+    }
+
+    let url = `https://wa.me/${phoneNumber}?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
     location.replace(url);
 }
 
-function submitData2() {
-    let nama = document.getElementById("name").value;
-    let umur = document.getElementById("umur").value;
-    let kota = document.getElementById("kota").value;
-    let wr = document.getElementById("WR").value;
-    let rr = document.getElementById("RR").value;
-    let rank = document.getElementById("rank").value;
-    let role = document.getElementById("role").value;
-    let id = document.getElementById("ID").value;
-    let url = `https://wa.me/6283145372436?text=Nama : ${nama}%0aUmur : ${umur}%0aAsal : ${kota}%0aWinrate : ${wr}%0aRank Tertinggi : ${rr}%0aRank Saat Ini : ${rank}%0aRole : ${role}%0aID ML : ${id}`;
-    location.replace(url);
+function redirectOnRefresh(subdomain, maindomain) {
+  window.addEventListener('beforeunload', function() {
+    if (window.location.hostname!== subdomain) {
+      window.history.replaceState({}, '', 'https://' + maindomain);
+    }
+  });
 }
